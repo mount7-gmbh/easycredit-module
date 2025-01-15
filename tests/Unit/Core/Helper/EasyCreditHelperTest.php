5e203e756830d7e6d49e15c460831ae7308ef5b1
Chris Smith <chrissmith@mount7.com>
2025-01-15 13:32:16 +0100

    dto and helper test added back in

---
+++ tests/Unit/Core/Helper/EasyCreditHelperTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Core\Helper;
+
+use OxidProfessionalServices\EasyCredit\Core\Helper\EasyCreditHelper;
+use PHPUnit\Framework\TestCase;
+
+final class EasyCreditHelperTest extends TestCase
+{
+
+    public function testHasPackstationFormatNormal()
+    {
+        $this->assertFalse(EasyCreditHelper::hasPackstationFormat('Teststraße', '7'));
+    }
+
+    public function testHasPackstationFormatNumericNoNumericPackStation1()
+    {
+        $this->assertFalse(EasyCreditHelper::hasPackstationFormat('014', ''));
+    }
+
+    public function testHasPackstationFormatNumericNoNumericPackStation2()
+    {
+        $this->assertFalse(EasyCreditHelper::hasPackstationFormat('', '014'));
+    }
+
+    public function testHasPackstationFormatNumericPackStation()
+    {
+        $this->assertTrue(EasyCreditHelper::hasPackstationFormat('014', '4711'));
+    }
+
+    public function testHasPackstationFormatNonNumericPackStation1()
+    {
+        $this->assertTrue(EasyCreditHelper::hasPackstationFormat('Packstation 014', ''));
+    }
+
+    public function testHasPackstationFormatNonNumericPackStation2()
+    {
+        $this->assertTrue(EasyCreditHelper::hasPackstationFormat('', 'Packstation 4711'));
+    }
+
+    public function testGetShopSystemCE()
+    {
+        $shop = new \stdClass();
+        $shop->oxshops__oxedition = new \stdClass();
+        $shop->oxshops__oxedition->value = 'CE';
+
+        $this->assertEquals('Community Edition', EasyCreditHelper::getShopSystem($shop));
+    }
+
+    public function testGetShopSystemPE()
+    {
+        $shop = new \stdClass();
+        $shop->oxshops__oxedition = new \stdClass();
+        $shop->oxshops__oxedition->value = 'PE';
+
+        $this->assertEquals('Professional Edition', EasyCreditHelper::getShopSystem($shop));
+    }
+
+    public function testGetShopSystemEE()
+    {
+        $shop = new \stdClass();
+        $shop->oxshops__oxedition = new \stdClass();
+        $shop->oxshops__oxedition->value = 'EE';
+
+        $this->assertEquals('Enterprise Edition', EasyCreditHelper::getShopSystem($shop));
+    }
+
+}
