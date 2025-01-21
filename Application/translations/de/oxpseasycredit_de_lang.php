cc20c1159f3ebabda4dda10b1d5a0f68ed266c91
Chris Smith <chris@cgsmith.net>
2025-01-21 14:24:37 +0100

    phpcs and phpstan updates

--- Application/translations/de/oxpseasycredit_de_lang.php
+++ Application/translations/de/oxpseasycredit_de_lang.php
@@ -1,17 +1,10 @@
 <?php
-/**
- * This Software is the property of OXID eSales and is protected
- * by copyright law - it is NOT Freeware.
+
+/*
+ * This file is part of OXID eSales AG EasyCredit module
+ * Copyright © OXID eSales AG. All rights reserved.
  *
- * Any unauthorized use of this software without a valid license key
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * @category      module
- * @package       easycredit
- * @author        OXID Professional Services
- * @link          http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2018
+ * Licensed under the GNU GPL v3 - See the file LICENSE for details.
  */
 
 $sLangName = 'Deutsch';
